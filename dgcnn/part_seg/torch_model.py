41d17bf280fcf17f63288bdb62b0102268df41c0
antianxu <antianxu@v.cluster.local>
2021-04-16 02:50:35 -0400

    update pytorch model

--- dgcnn/part_seg/torch_model.py
+++ dgcnn/part_seg/torch_model.py
@@ -145,66 +145,110 @@ class PointNet(nn.Module):
 
 
 class DGCNN(nn.Module):
-    def __init__(self, args, output_channels=40):
+    def __init__(self, args, input_dim, part_num):
         super(DGCNN, self).__init__()
         self.args = args
         self.k = 20
-        
+
+        #TODO: add bn_decay to args
+        self.bn_decay = args.bn_decay
+
         #TODO: batchnorm decay and weight decay
         self.bn1 = nn.BatchNorm2d(64)
-        self.bn2 = nn.BatchNorm2d(64)
-        self.bn3 = nn.BatchNorm2d(128)
-        self.bn4 = nn.BatchNorm2d(256)
-        self.bn5 = nn.BatchNorm1d(args.emb_dims)
+        self.bn2 = nn.BatchNorm2d(128)
+        self.bn3 = nn.BatchNorm2d(256)
+        self.bn4 = nn.BatchNorm2d(512)
+        self.bn5 = nn.BatchNorm2d(1024)
 
-        self.transform_conv1 = nn.Sequential(nn.Conv2d(6, 64, kernel_size=1, bias=False),
+        self.transform_conv1 = nn.Sequential(nn.Conv2d(input_dim, 64, kernel_size=1, bias=True),
                                    self.bn1,
                                    nn.ReLU())
 
-        self.transform_conv2 = nn.Sequential(nn.Conv2d(64, 128, kernel_size=1, bias=False),
-                                   self.bn1,
+        self.transform_conv2 = nn.Sequential(nn.Conv2d(64, 128, kernel_size=1, bias=True),
+                                   self.bn2,
                                    nn.ReLU())
         
-        self.transform_conv3 = nn.Sequential(nn.Conv2d(128, 1024, kernel_size=1, bias=False),
+        self.transform_conv3 = nn.Sequential(nn.Conv2d(128, 1024, kernel_size=1, bias=True),
+                                   self.bn5,
+                                   nn.ReLU())
+
+
+        #TODO: check tf_util: fully connected is not nn.linear
+        #TODO: check this dimension
+        self.transform_fc1 = nn.Sequential(nn.Linear(1024, 512, bias=True),
+                                            self.bn4,
+                                            nn.ReLU())
+
+        self.transform_fc2 = nn.Sequential(nn.Linear(512, 256, bias=True),
+                                            self.bn3,
+                                            nn.ReLU())
+
+        self.transform_fc3 = nn.Linear(256, 9, bias=True)
+
+        self.conv1 = nn.Sequential(nn.Conv2d(input_dim, 64, kernel_size=1, bias=True),
+                                   self.bn1,
+                                   nn.ReLU())
+        self.conv2 = nn.Sequential(nn.Conv2d(64, 64, kernel_size=1, bias=True),
                                    self.bn1,
                                    nn.ReLU())
 
-        #TODO: check tf_util: fully connected is not linear
-        self.transform_fc1 = nn.Linear(args.emb_dims*2, 512, bias=False)
-        self.transform_fc2 = nn.Linear(512, 256, bias=False)
-
-        self.conv1 = nn.Sequential(nn.Conv2d(6, 64, kernel_size=1, bias=False),
+        self.conv3 = nn.Sequential(nn.Conv2d(64, 64, kernel_size=1, bias=True),
                                    self.bn1,
                                    nn.ReLU())
-        self.conv2 = nn.Sequential(nn.Conv2d(64, 64, kernel_size=1, bias=False),
+
+        self.conv4 = nn.Sequential(nn.Conv2d(64, 64, kernel_size=1, bias=True),
                                    self.bn1,
-                                   nn.ReLU()
+                                   nn.ReLU())
+
+        self.conv5 = nn.Sequential(nn.Conv2d(64, 64, kernel_size=1, bias=True),
+                                   self.bn1,
+                                   nn.ReLU())
+
+        self.conv6 = nn.Sequential(nn.Conv2d(64*3, 64, kernel_size=1, bias=True),
+                                   self.bn1,
+                                   nn.ReLU(),
+                                   nn.MaxPool2d((num_points,1), stride=2))
+
+        self.conv7 = nn.Sequential(nn.Conv2d(64, 256, kernel_size=1, bias=True),
+                                   self.bn3,
+                                   nn.ReLU(),
+                                   nn.Dropout(p=0.4)
                                    )
 
-        self.conv3 = nn.Sequential(nn.Conv2d(64, 64, kernel_size=1, bias=False),
-                                   self.bn1,
+        self.conv8 = nn.Sequential(nn.Conv2d(256, 256, kernel_size=1, bias=True),
+                                   self.bn3,
+                                   nn.ReLU(),
+                                   nn.Dropout(p=0.4)
+                                   )
+
+        self.conv9 = nn.Sequential(nn.Conv2d(256, 128, kernel_size=1, bias=True),
+                                   self.bn2,
+                                   nn.ReLU())
+        
+        self.conv10 = nn.Sequential(nn.Conv2d(128, part_num, kernel_size=1, bias=True),
                                    nn.ReLU())
 
-        self.conv4 = nn.Sequential(nn.Conv2d(64, 64, kernel_size=1, bias=False),
-                                   self.bn1,
-                                   nn.ReLU())
-    
-        self.linear1 = nn.Linear(args.emb_dims*2, 512, bias=False)
-        self.bn6 = nn.BatchNorm1d(512)
-        self.dp1 = nn.Dropout(p=args.dropout)
-        self.linear2 = nn.Linear(512, 256)
-        self.bn7 = nn.BatchNorm1d(256)
-        self.dp2 = nn.Dropout(p=args.dropout)
-        self.linear3 = nn.Linear(256, output_channels)
 
-    def input_transform_net(edge_feature, is_training, bn_decay=None, K=3, is_dist=False):
+    def input_transform_net(edge_feature, bn_decay=None, K=3, is_dist=False):
         net = self.transform_conv1(edge_feature)
         net = self.transform_conv2(net)
         net = x.max(dim=-2, keepdim=True)[0]
         net = self.transform_conv3(net)
 
-        num_points = edge_feature.size(1)
+        batch_size, num_points, _ = edge_feature.size()
         net = nn.MaxPool2d((num_points, 1), stride=2)
+        net = torch.reshape(net, [batch_size, -1])
+
+        # TODO: weight decay for the fc layers
+        net = self.transform_fc1(net)
+        net = self.transform_fc2(net)
+
+        # TODO: check the added bias
+        net = self.transform_fc3(net)
+        net += torch.tensor(np.eye(K).flatten())
+
+        tranform = torch.reshape(net, (batch_size, K, K))
+        return transform
 
     def forward(self, x):
         batch_size = x.size(0)
@@ -215,34 +259,49 @@ class DGCNN(nn.Module):
         nn_idx = knn(x, k=self.k)
         
         edge_feature = get_edge_feature(x, k=self.k, nn_idx=nn_idx)
-        transform
+        transform = input_transform_net(edge_feature, bn_decay=bn_decay)
+        point_cloud_transformed = torch.matmul(x, transform)
 
-        x = self.conv1(x)
+        input_image = torch.unsqueeze(point_cloud_transformed, -1)
+
+        nn_idx = knn(x, k=self.k)
+        edge_feature = get_edge_feature(input_image, k=self.k, nn_idx=nn_idx)
+
+        x = self.conv1(edge_feature)
         x = self.conv2(x)
         x1 = x.max(dim=-2, keepdim=True)[0]
 
+        nn_idx = knn(x1, k=self.k)
         x = get_edge_feature(x1, k=self.k)
-        x = self.conv2(x)
-        x2 = x.max(dim=-1, keepdim=False)[0]
-
-        x = get_graph_feature(x2, k=self.k)
         x = self.conv3(x)
-        x3 = x.max(dim=-1, keepdim=False)[0]
-
-        x = get_graph_feature(x3, k=self.k)
         x = self.conv4(x)
-        x4 = x.max(dim=-1, keepdim=False)[0]
+        x2 = x.max(dim=-2, keepdim=True)[0]
 
-        x = torch.cat((x1, x2, x3, x4), dim=1)
-
+        nn_idx = knn(x2, k=self.k)
+        x = get_edge_feature(x2, k=self.k, nn_idx=nn_idx)
         x = self.conv5(x)
-        x1 = F.adaptive_max_pool1d(x, 1).view(batch_size, -1)
-        x2 = F.adaptive_avg_pool1d(x, 1).view(batch_size, -1)
-        x = torch.cat((x1, x2), 1)
+        x3 = x.max(x, dim=-2, keepdim=True)[0]
 
-        x = F.leaky_relu(self.bn6(self.linear1(x)), negative_slope=0.2)
-        x = self.dp1(x)
-        x = F.leaky_relu(self.bn7(self.linear2(x)), negative_slope=0.2)
-        x = self.dp2(x)
-        x = self.linear3(x)
-        return x
+        out_max = self.conv6(torch.cat((x1, x2, x3), dim=-1))
+
+        # remove one_hot_label_expand from part_seg_model
+
+        expand = torch.tile(out_max, [1, num_point, 1, 1])
+        concat = torch.cat((expand, x1, x2, x3), dim=3)
+    
+        result = self.conv7(concat) #includes dropout
+        result = self.conv8(result)
+        result = self.conv9(result)
+        result = self.conv10(result)
+
+        result = torch.reshape(result, (batch_size, num_points, part_num))
+        # x = self.conv(x)
+        # x3 = x.max(dim=-1, keepdim=False)[0]
+
+        # x = get_graph_feature(x3, k=self.k)
+        # x = self.conv4(x)
+        # x4 = x.max(dim=-1, keepdim=False)[0]
+
+        # x = torch.cat((x1, x2, x3, x4), dim=1)
+
+        return result
